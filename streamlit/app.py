c6899b2dd6bf73ce146f9efc51556fd66bffb2ca
rephrain <rephrain1@example.com>
2026-04-20 09:05:50 +0700

    test123

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
  
 st.write("""
 # My first app
@@ -7,6 +8,13 @@ Hello *world!*
 satu dua tiga
 """)
  
-df = pd.read_csv("streamlit/children_thr_data.csv")
+# df = pd.read_csv("streamlit/children_thr_data.csv")
+# df["age"] = df["age"].astype(int)
+# st.line_chart(data=df, x="name", y="age")
+
+
+curr_dir = Path(__file__).parent
+
+df = pd.read_csv(f"{curr_dir}/children_thr_data.csv")
 df["age"] = df["age"].astype(int)
 st.line_chart(data=df, x="name", y="age")
